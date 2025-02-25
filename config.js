6284d93ad2ece9167c96a1482a020aae1ac42cdb
raideneii76 <raideneii188@gmail.com>
2025-02-25 22:34:31 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ global.owner = ['6738180886/6738685635'] // owner number
 global.connect = true // Ubah ke false jika ingin mengkoneksikan bot menggunakan QrCode
 global.url = "https://t.me/devorsix_exposed"
 global.url2 = "https://t.me/devorsix_exposed"
-global.packname = "anonymously"
-global.author = "kntl"
+global.packname = "devorsixcore"
+global.author = "devorsixcore"
