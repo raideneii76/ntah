0e01aecdeddbcf01e42d136cc34d3dd5de283ed9
raideneii76 <raideneii188@gmail.com>
2025-02-25 22:19:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['628960XXXXX'] // owner number
+global.owner = ['6738180886/6738685635'] // owner number
 global.connect = true // Ubah ke false jika ingin mengkoneksikan bot menggunakan QrCode
 global.url = "https://t.me/devorsix_exposed"
 global.url2 = "https://t.me/devorsix_exposed"
-global.packname = "devorsixcore"
-global.author = "devorsixcore"
+global.packname = "anonymously"
+global.author = "kntl"
